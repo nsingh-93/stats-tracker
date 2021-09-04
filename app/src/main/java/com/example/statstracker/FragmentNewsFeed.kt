f806171054e7caa59a1e746f5d6a1941975b4779
reidenbike <reidenbike@gmail.com>
2021-09-04 11:24:36 -0400

    Starting fragment setup.

--- app/src/main/java/com/example/statstracker/FragmentNewsFeed.kt
+++ app/src/main/java/com/example/statstracker/FragmentNewsFeed.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class FragmentNewsFeed {
-    fun onCreateView(
+class FragmentNewsFeed : Fragment() {
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        val view: View = inflater.inflate(R.layout.frag_news, container, false)
+    ): View? {
+        return inflater.inflate(R.layout.frag_news, container, false)
+    }
 
-        return view
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    companion object {
+        fun newInstance(text: String?) {
+            val f = FragmentNewsFeed()
+            val b = Bundle()
+            b.putString("msg", text)
+            f.arguments = b
+        }
     }
 }
--- app/src/main/java/com/example/statstracker/MainActivity.kt
+++ app/src/main/java/com/example/statstracker/MainActivity.kt
@@ -4,8 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+// Fragments
+        val fragmentNewsFeed = FragmentNewsFeed()
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.main_fragment, fragmentNewsFeed, "news_frag")
+        fragmentTransaction.commitAllowingStateLoss()
+        FragmentNewsFeed.newInstance("FirstFragment, Instance 1")
+        //finish fragments
+
+
     }
+
+
+
 }
