f40195bc2cdf7b4258f9143dabc48f99091eae36
reidenbike <reidenbike@gmail.com>
2021-09-05 13:16:12 -0400

    Implemented ViewPager2 in MainActivity.kt

---
+++ app/src/main/java/com/example/statstracker/FragmentScores.kt
@@ -0,0 +1,64 @@
+package com.example.statstracker
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+
+class FragmentScores : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val newsView: View = inflater.inflate(R.layout.frag_news, container, false)
+        val newsRecyclerView: RecyclerView = newsView.findViewById(R.id.newsRecyclerView)
+
+        val newsList: ArrayList<News> = ArrayList()
+
+        for (i in 0 until 10) {
+            val news = object : News() {}
+
+            news.setHeadline("Headline $i")
+            news.setBody("Description $i")
+            news.setUrl("https://www.nhl.com/")
+            news.setTime("9/4/2021")
+            news.setImage("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Capitals-Maple_Leafs_%2834075134291%29.jpg/1200px-Capitals-Maple_Leafs_%2834075134291%29.jpg")
+
+            newsList.add(news)
+
+            Log.i("NEWS",(if (news.getHeadline() != null) news.getHeadline() else "empty")!!)
+        }
+
+        Log.i("NEWS",
+            (if (newsList[0].getHeadline() != null) newsList[0].getHeadline() else "empty")!!
+        )
+
+        val newsRecyclerAdapter = NewsFeedRecyclerAdapter(newsList, requireContext())
+        newsRecyclerView.adapter = newsRecyclerAdapter
+
+        val linearLayoutManager = LinearLayoutManager(requireContext())
+        newsRecyclerView.layoutManager = linearLayoutManager
+
+        return newsView
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    companion object {
+        fun newInstance(text: String?) {
+            val f = FragmentScores()
+            val b = Bundle()
+            b.putString("fragment", text)
+            f.arguments = b
+        }
+    }
+}
