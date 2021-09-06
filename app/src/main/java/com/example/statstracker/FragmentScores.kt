f88044c043e3a0fbac35a5d81d8ac04d3a23a26d
Neil <neils@vt.edu>
2021-09-06 11:00:45 -0400

    Finished up recycler view with requests to get scores

--- app/src/main/java/com/example/statstracker/FragmentScores.kt
+++ app/src/main/java/com/example/statstracker/FragmentScores.kt
@@ -10,13 +10,23 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import java.util.ArrayList
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.statstracker.newsapi.NewsResult
+import com.google.gson.Gson
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FragmentScores : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val gson = Gson()
+        val stringRequest: StringRequest?
+
         val scoresView: View = inflater.inflate(R.layout.frag_scores, container, false)
         val scoresRecyclerView: RecyclerView = scoresView.findViewById(R.id.scoresRecyclerView)
 
@@ -32,7 +42,94 @@ class FragmentScores : Fragment() {
         val linearLayoutManager = LinearLayoutManager(requireContext())
         scoresRecyclerView.layoutManager = linearLayoutManager
 
+        val url = "https://statsapi.web.nhl.com/api/v1/schedule"
+
+        val cal = Calendar.getInstance()
+        val formatter = SimpleDateFormat("yyyy-MM-dd",  Locale.getDefault())
+        val startDate = cal.add(Calendar.DATE, -1)
+        val startDateAsString = "2021-12-25"//formatter.format(startDate)
+        val endDate = cal.add(Calendar.DATE, 1)
+        val endDateAsString = "2021-12-28"//formatter.format(endDate)
+
+        val query = "$url?startDate=$startDateAsString&endDate=$endDateAsString"
+
+        val requestQueue = Volley.newRequestQueue(requireContext())
+        stringRequest = object : StringRequest(
+            Method.GET, query,
+            Response.Listener { response ->
+                val scoresJsonObject = JSONObject(response)
+
+                val gsonParse = gson.fromJson(scoresJsonObject.toString(), ScoresResult::class.java)
+
+                var totalGames = gsonParse.totalGames
+
+                if(totalGames != 0){
+
+                    for (dates in gsonParse.dates) {
+                        for(game in dates.games) {
+                            val score = object : Scores() {}
+
+                            val home = game.teams.home
+                            val away = game.teams.away
+
+                            score.setHomeTeam(home.team.name)
+                            score.setHomeTeamAbbr(CreateTeamAbbreviation(score.getHomeTeam()!!))
+                            score.setHomeTeamScore(home.score)
+
+                            val homeTeamStreak = home.leagueRecord.wins.toString() + "-" + home.leagueRecord.losses.toString() + "-" + home.leagueRecord.ot.toString()
+                            score.setHomeTeamStreak(homeTeamStreak)
+
+                            score.setAwayTeam(away.team.name)
+                            score.setAwayTeamAbbr(CreateTeamAbbreviation(score.getAwayTeam()!!))
+                            score.setAwayTeamScore(away.score)
+
+                            val awayTeamStreak = away.leagueRecord.wins.toString() + "-" + away.leagueRecord.losses.toString() + "-" + away.leagueRecord.ot.toString()
+                            score.setAwayTeamStreak(awayTeamStreak)
+
+                            score.setGameDate(dates.date)
+                            score.setGameStatus(game.status.abstractGameState)
+
+                            scoresList.add(score)
+                        }
+                    }
+                }
+
+                scoresRecyclerAdapter.notifyDataSetChanged()
+
+                requestQueue.stop()
+            },
+            Response.ErrorListener { error ->
+                Log.d("NEWS", "error => $error")
+                requestQueue.stop()
+            }
+        ) {
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"]="Mozilla/5.0"
+                return headers
+            }
+        }
+
+        // Add the request to the RequestQueue.
+        //TODO Uncomment request below
+        //requestQueue.add(stringRequest)
+
+        //-------------------------------------------------
+
+
         return scoresView
+    }
+
+    fun CreateTeamAbbreviation(teamName:String):String
+    {
+        var words = teamName.split(" ")
+
+        return if (words.size == 2) {
+            words[0].substring(0, 3).uppercase()
+        } else {
+            (words[0].substring(0, 1) + words[1].substring(0, 1) + words[2].substring(0, 1)).uppercase()
+        }
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/com/example/statstracker/ScoresResult.kt
@@ -0,0 +1,93 @@
+package com.example.statstracker
+
+import java.util.*
+
+data class ScoresResult(
+    var copyright:String,
+    var totalItems:Int,
+    var totalEvents:Int,
+    var totalGames:Int,
+    var totalMatches:Int,
+    var metaData:MetaData,
+    var wait:Int,
+    var dates:List<ScoreDates>,
+
+)
+
+data class MetaData(
+    var timeStamp:String
+)
+
+data class ScoreDates(
+    var date:Date,
+    var totalItems:Int,
+    var totalEvents:Int,
+    var totalGames:Int,
+    var totalMatches:Int,
+    var games:List<Games>,
+    var events:Events,
+    var matches:Matches
+)
+
+data class Games(
+    var gamePK:String,
+    var link:String,
+    var gameType:String,
+    var season:String,
+    var gameDate:Date,
+    var status:GameStatus,
+    var teams:GameTeams,
+    var venue:GameVenue,
+    var content:GameContent
+)
+
+data class GameStatus(
+    var abstractGameState:String,
+    var codedGameState:String,
+    var detailedState:String,
+    var statusCode:String,
+    var startTimeTBD:Boolean
+)
+
+data class GameTeams(
+    var away:TeamStats,
+    var home:TeamStats
+)
+
+data class TeamStats(
+    var leagueRecord:LeagueRecord,
+    var score:Int,
+    var team:Team
+)
+
+data class LeagueRecord(
+    var wins:Int,
+    var losses:Int,
+    var ot:Int,
+    var leagueType:String
+)
+
+data class Team(
+    var id:Int,
+    var name:String,
+    var link:String
+)
+
+data class GameVenue(
+    var id:Int,
+    var name:String,
+    var link:String
+)
+
+data class GameContent(
+    var link:String
+)
+
+data class Events(
+    var eventType:String
+)
+
+data class Matches(
+    var matchType:String
+)
+
